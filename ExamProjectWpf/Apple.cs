692deffbb9e52fa45729638c8de8235f353c905a
JekaLion <djoni-01@mail.ru>
2018-02-02 17:45:06 +0600

    init

---
+++ ExamProjectWpf/Apple.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamProjectWpf
+{
+    public class Apple
+    {
+        public Guid Id { get; set; }
+        public string City { get; set; }
+        public int Price { get; set; }
+        public int Loss { get; set; }
+        public int Profit { get; set; }
+        private int delivery = 10;       //доставка
+        private int cultivation = 20;    //выращивание
+        private int marriage = 10;       //брак
+
+        public int CalculateProfit()
+        {
+            Profit = Price - Loss;
+            return Profit;
+        }
+
+        public int CalculateLoss()
+        {
+            Loss = (delivery + cultivation + marriage);
+            return Loss;
+        }
+    }
+}
---
+++ ExamProjectWpf/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExamProjectWpf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Apple> apples;
+        private string path;
+        public MainWindow()
+        {
+            InitializeComponent();
+            path = Directory.GetCurrentDirectory() + "/save.json";
+            FileRead();
+            mainDataGrid.ItemsSource = apples;
+        }
+
+        private void MainDataGridCellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            foreach (Apple i in apples)
+            {
+                i.CalculateLoss();
+                i.CalculateProfit();
+            }
+            Apple p = apples[0];
+            FileWrite();
+        }
+
+        #region JsonFile
+        private void FileRead()
+        {
+            using (FileStream fileStream = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                using (StreamReader stream = new StreamReader(fileStream))
+                {
+                    using (JsonTextReader reader = new JsonTextReader(stream))
+                    {
+                        JsonSerializer serializer = new JsonSerializer();
+                        apples = serializer.Deserialize<ObservableCollection<Apple>>(reader);
+                        if (apples == null) apples = new ObservableCollection<Apple>();
+                    }
+                }
+            }
+        }
+
+        private void FileWrite()
+        {
+            using (FileStream fileStream = new FileStream(path, FileMode.Create))
+            {
+                using (StreamWriter stream = new StreamWriter(fileStream))
+                {
+                    using (JsonTextWriter writer = new JsonTextWriter(stream))
+                    {
+                        JsonSerializer serializer = new JsonSerializer();
+                        serializer.Serialize(writer, apples);
+                    }
+                }
+            }
+        }
+        #endregion
+    }
+}
